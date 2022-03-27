0596711fd7256096ca2215c7ec5bd4213ad85aa7
FrBrenno <72556312+FrBrenno@users.noreply.github.com>
2022-03-27 12:16:00 +0200

    Code Fonctionne, manque esquive

--- main.py
+++ main.py
@@ -9,16 +9,6 @@ import math
 import time
 
 def my_ai(gamestate: SnapshotData, my_data: typing.Dict) -> Command:
-	def moveTo(x,y,xMe, yMe):
-		if x==xMe:
-			return MoveCommand((0.0,math.copysign(1,yMe-y),0.0))
-		if y==yMe:
-			return MoveCommand((math.copysign(1,xMe-x),0.0,0.0))
-		return MoveCommand(((x-xMe)/math.sqrt((xMe-x)*(xMe-x)+(yMe-y)*(yMe-y)), (y-yMe)/math.sqrt((xMe-x)*(xMe-x)+(yMe-y)*(yMe-y)),0.0))
-	def inZone(down,up,left,right,x,y):
-		if x < right and x > left and y < up and y > down:
-			return True
-		return False
 	scoreMe = gamestate.controlled_player.score
 	scoreHim = gamestate.other_players[0].score
 	healthMe = gamestate.controlled_player.health
@@ -27,32 +17,104 @@ def my_ai(gamestate: SnapshotData, my_data: typing.Dict) -> Command:
 	yMe = gamestate.controlled_player.position[1]
 	xHim = gamestate.other_players[0].position[0]
 	yHim = gamestate.other_players[0].position[1]
-	gamestate.projectiles
-	meCenter = inZone(-100,100,-100,100,xMe, yMe)
-	himCenter = inZone(-100,100,-100,100,xHim, yHim)
-	if healthHim < 0:
-		if scoreHim < scoreMe:
-			if inZone(216,275,216,275,xMe,yMe):
-				return ShootCommand(-90)
-			else :
-				return moveTo(250,250,xMe,yMe)
+ 
+	def moveTo(x,y):
+		if y==yMe and x==xMe:
+			return ShootCommand(0.0)
+		if x==xMe:
+			return MoveCommand((0.0,math.copysign(1.0,y-yMe),0.0))
+		if y==yMe:
+			return MoveCommand((math.copysign(1.0,x-xMe),0.0,0.0))
+		return MoveCommand(((x-xMe)/math.sqrt((xMe-x)*(xMe-x)+(yMe-y)*(yMe-y)), (y-yMe)/math.sqrt((xMe-x)*(xMe-x)+(yMe-y)*(yMe-y)),0.0))
+	
+	def zone(x,y):
+			if y > 216:
+				if x < -216:
+					return "CornerUpLeft"
+				elif x < -34:
+					return "CornerUpAlmostLeft"
+				elif x < 216:
+					return "CenterUp"
+				else:
+					return "CornerUpRight"
+			elif y > 0 and x < -216:
+				return "CornerLeftAlmostUp"
+			elif y < 0 and x > 216:
+				return "CornerRightAlmostDown"
+			elif y < -216:
+				if x > 216:
+					return "CornerDownRight"
+				elif x > 34:
+					return "CornerDownAlmostRight"
+				elif x >-216:
+					return "CenterDown"
+				else:
+					return "CornerDownLeft"
+			elif x < 100 and x >-100 and y > -100 and y < 100:
+				return "Center"
+			return "Middle"
+
+	"""def inDanger(projectile):
+		xPro = projectile.position[0]
+		yPro = projectile.position[1]
+		xSpeed = projectile.speed[0]
+		ySpeed = projectile.speed[1]
+		angle = (-math.copysign(1,xPro-xMe)*90+90)+arctan(ySpeed/xSpeed)*180/3.14
+		if (xMe - xPro)*(xMe - xPro)+(yMe - yPro)*(yMe - yPro) <36000 and angle > :
+			pass
+	  
+	def esquive():
+		for projectile in gamestate.projectiles:
+			if inDanger(projectile):
+				projectile.position
+			else:
+				pass"""
+	
+	
+	if my_data.get('first') is None:
+		if xMe < -20.0:
+			my_data['first'] = 1
 		else:
-			return moveTo(0,0,xMe,yMe)
+			my_data['first'] = 0
+	meCenter = zone(xMe, yMe) == "Center"
+	himCenter = zone(xHim, yHim) == "Center"
+	if healthHim < 0:
+		if scoreHim < scoreMe and not zone(xMe,yMe) == "CornerLeftAlmostUp" and not zone(xMe,yMe) == "CornerDownAlmostRight":
+			if zone(xMe,yMe) == "CornerUpRight":
+				if my_data.get('first') == 1:
+					return ShootCommand(-90)
+				else:
+					return ShootCommand(180)
+			else:
+				return moveTo(250,250)
+		elif zone(xMe,yMe) == "CornerUpLeft":
+			return moveTo(-250,0)
+		elif zone(xMe,yMe) == "CornerDownRight":
+			return moveTo(250,0)
+		return moveTo(0,0)
 	elif meCenter and not himCenter:
 		return ShootCommand((-math.copysign(1,xHim-xMe)*90+90)+arctan((yHim-yMe)/(xHim-xMe))*180/3.14)
 	elif not meCenter and not himCenter:
-		if (inZone(-275,0,216,275,xMe,yMe) and inZone(0,275,-275,-216,xHim,yHim)) or (inZone(-275,0,216,275,xHim,yHim) and inZone(0,275,-275,-216,xMe,yMe)):
-			return moveTo(0,0,xMe,yMe)
-		if (inZone(216,275,216,275,xMe,yMe) and inZone(-275,0,216,275,xHim,yHim)) or (inZone(216,275,216,275,xMe,yMe) and inZone(-275,0,-275,-216,xHim,yHim)):
-			return ShootCommand((-math.copysign(1,xHim-xMe)*90+90)+arctan((yHim-yMe)/(xHim-xMe))*180/3.14)
-		else: 
-			moveTo(-250,0,xMe, yMe)
+		if (zone(xMe,yMe) == "CornerUpLeft" and (zone(xHim,yHim) == "CornerUpRight" or zone(xHim,yHim) == "CornerUp")):
+			return moveTo(-250,0)
+		elif (zone(xMe,yMe) == "CornerUpLeft" and (zone(xHim,yHim) == "CornerUpAlmostLeft" or zone(xHim,yHim) == "CornerDownLeft")):
+			return moveTo(0,250)
+		elif (zone(xMe,yMe) == "CornerDownRight" and (zone(xHim,yHim) == "CornerUpRight" or zone(xHim,yHim) == "CornerRightAlmostDown")):
+			return moveTo(0,-250)
+		elif (zone(xMe,yMe) == "CornerDownRight" and (zone(xHim,yHim) == "CornerDownLeft" or zone(xHim,yHim) == "CornerUpAlmostLeft")):
+			return moveTo(250,0)
+		else:
+			if zone(xMe,yMe) == "CornerUpLeft" or zone(xMe,yMe) == "CornerLeftAlmostUp":
+				return moveTo(-250,0)
+			if zone(xMe,yMe) == "CornerDownRight" or zone(xMe,yMe) == "CornerRightAlmostDown":
+				return moveTo(250,0)
+			return moveTo(0,0)
 	elif not meCenter and himCenter:
-		if inZone(-275,0,216,275,xMe,yMe):
-			return moveTo(250,0,xMe,yMe)
-		if inZone(0,275,-275,-216,xMe,yMe):
-			return moveTo(-250,0,xMe,yMe)
-		return moveTo(0,0,xMe,yMe)
+		if zone(xMe,yMe) == "CornerUpLeft" or zone(xMe,yMe) == "CornerLeftAlmostUp":
+			return moveTo(-250,0)
+		if zone(xMe,yMe) == "CornerDownRight" or zone(xMe,yMe) == "CornerRightAlmostDown":
+			return moveTo(250,0)
+		return moveTo(0,0)
 	elif meCenter and himCenter:
 		return ShootCommand((-math.copysign(1,xHim-xMe)*90+90)+arctan((yHim-yMe)/(xHim-xMe))*180/3.14)
 	"""
@@ -61,18 +123,7 @@ def my_ai(gamestate: SnapshotData, my_data: typing.Dict) -> Command:
 			data['right'] = 1
 		else:
 			data['right'] += 1
-
-	x = gamestate.controlled_player.position[0]
-	y = gamestate.controlled_player.position[1]
-	increase_right(my_data)
-	if x < 0:
-		return MoveCommand((1.0, 0.0, 0.0))
-	elif y > 0:
-		return MoveCommand((0.0, -1.0, 0.0))
-	else:
-		return ShootCommand(45.0)
-#		return MoveCommand((0.0, 0.0, 0.0))
-  	"""
+	"""
 	return ShootCommand(45.0)
 
 def idle_ai(gamestate: SnapshotData, my_data: typing.Dict) -> Command:
@@ -83,7 +134,7 @@ if __name__ == '__main__':
 	simulation_list = []
 	used_port = 2049
 
-	simulation = GameSimulation('java', game_time=20.0, ai_time=150.0, commands_per_second=4.0, port=used_port)
+	simulation = GameSimulation('java', game_time=60.0, ai_time=150.0, commands_per_second=4.0, port=used_port)
 
 	first_agent_data = {}
 	second_agent_data = {}
@@ -100,4 +151,4 @@ if __name__ == '__main__':
 
 	for agent_data in data:
 		print(f'Agent data: {agent_data}')
-        
+		
